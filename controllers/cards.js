cbb2f0bb3b6f9beeebf14d131099978df752f472
Artsiom Prystupa <pristupalol@gmail.com>
2023-06-19 23:24:46 +0300

    вторая проверка

--- controllers/cards.js
+++ controllers/cards.js
@@ -20,7 +20,7 @@ module.exports.findCards = (req, res) => {
 
 module.exports.deleteCardId = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
-
+    .orFail()
     .then((card) => check(card, res))
     .catch((err) => processingError(err, res));
 };
@@ -30,7 +30,7 @@ module.exports.likeCard = (req, res) => Card.findByIdAndUpdate(
   { $addToSet: { likes: req.user._id } },
   { new: true },
 )
-
+  .orFail()
   .then((card) => check(card, res))
   .catch((err) => processingError(err, res));
 
@@ -38,5 +38,6 @@ module.exports.dislikeCard = (req, res) => Card.findByIdAndUpdate(
   req.params.cardId,
   { $pull: { likes: req.user._id } },
 )
+  .orFail()
   .then((card) => check(card, res))
   .catch((err) => processingError(err, res));
